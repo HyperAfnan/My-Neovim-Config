35fd04ba8097549e7c27570275db961fb6418a0d
HyperAfnan <afnanmalik2712@gmail.com>
2024-06-03 23:46:26 +0530

    feat(git): Added nvim-gitsigns

--- lua/afnan/lazyy.lua
+++ lua/afnan/lazyy.lua
@@ -11,14 +11,14 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "afnan.plugins" }}, {
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-  },
+require("lazy").setup({ { import = "afnan.plugins" } }, {
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
 })
 vim.cmd("colorscheme onedark")
 vim.g.netrw_liststyle = 3
--- lua/afnan/plugins/lsp.lua
+++ lua/afnan/plugins/lsp.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: param-type-mismatch
 return {
 	{
 		"neovim/nvim-lspconfig",
@@ -10,26 +11,43 @@ return {
 			local cmp_nvim_lsp = require("cmp_nvim_lsp")
 			local lspconfig = require("lspconfig")
 			local lightbulb = require("nvim-lightbulb")
-         local border = { " ", " ", " ", " ", " ", " ", " ", " " }
+			local border = { " ", " ", " ", " ", " ", " ", " ", " " }
 			local capabilities = cmp_nvim_lsp.default_capabilities()
 
 			local function on_attach(client, bufnr)
+				local function set_keymap(mode, lhs, rhs)
+					vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, { silent = true, noremap = true })
+				end
+
+				set_keymap("n", "K", ":lua vim.lsp.buf.hover()<CR>")
+				set_keymap("n", "gd", ":lua vim.lsp.buf.definition()<CR>")
+				set_keymap("n", "gI", ":lua vim.lsp.buf.implementation()<CR>")
+				set_keymap("n", "gk", ":lua vim.diagnostic.goto_next()<CR>")
+				set_keymap("n", "gj", ":lua vim.diagnostic.goto_prev()<CR>")
+				set_keymap("n", "gR", ":lua vim.lsp.buf.references()<CR>")
+				set_keymap("n", "gr", ":lua vim.lsp.buf.rename()<CR>")
+				set_keymap("n", "ga", ":lua vim.lsp.buf.code_action()<CR>")
+				set_keymap("i", "<C-s>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+
+				-- document highlights
 				if client.resolved_capabilities.document_highlight then
 					vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
-					vim.api.nvim_create_autocmd({
+					vim.api.nvim_create_autocmd("CursorHold", {
 						callback = function()
 							vim.lsp.buf.document_highlight()
 						end,
 						buffer = bufnr,
-					}, "CursorHold")
-					vim.api.nvim_create_autocmd({
+					})
+					vim.api.nvim_create_autocmd("CursorMoved", {
 						callback = function()
 							vim.lsp.buf.clear_references()
 						end,
 						buffer = bufnr,
-					}, "CursorMoved")
+					})
 				end
+				vim.notify(client.name .. " is Started", "INFO", {})
 			end
+
 			lightbulb.setup({
 				autocmd = { enabled = true },
 				sign = { enable = false },
@@ -144,172 +162,103 @@ return {
 				},
 			})
 
-
-local codes =  {
-		no_matching_function = {
-			message = " Can't find a matching function",
-			"redundant-parameter",
-			"ovl_no_viable_function_in_call",
-		},
-		empty_block = { message = " That shouldn't be empty here", "empty-block" },
-		missing_symbol = {
-			message = " Here should be a symbol",
-			"miss-symbol",
-		},
-		expected_semi_colon = {
-			message = " Remember the `;` or `,`",
-			"expected_semi_declaration",
-			"miss-sep-in-table",
-			"invalid_token_after_toplevel_declarator",
-		},
-		redefinition = {
-			message = " That variable was defined before",
-			"redefinition",
-			"redefined-local",
-		},
-		no_matching_variable = {
-			message = " Can't find that variable",
-			"undefined-global",
-			"reportUndefinedVariable",
-		},
-		trailing_whitespace = {
-			message = " Remove trailing whitespace",
-			"trailing-whitespace",
-			"trailing-space",
-		},
-		unused_variable = {
-			message = " Don't define variables you don't use",
-			"unused-local",
-		},
-		unused_function = {
-			message = " Don't define functions you don't use",
-			"unused-function",
-		},
-		useless_symbols = {
-			message = " Remove that useless symbols",
-			"unknown-symbol",
-		},
-		wrong_type = {
-			message = " Try to use the correct types",
-			"init_conversion_failed",
-		},
-		undeclared_variable = {
-			message = " Have you delcared that variable somewhere?",
-			"undeclared_var_use",
-		},
-		lowercase_global = {
-			message = " Should that be a global? (if so make it uppercase)",
-			"lowercase-global",
-		},
-	}
-
-local function format(diagnostic)
-		local code = diagnostic.user_data.lsp.code
-		for _, table in pairs(codes) do
-			if vim.tbl_contains(table, code) then
-				return table.message
+			local function prefix(diagnostic, i, total)
+				local icon, highlight
+				if diagnostic.severity == 1 then
+					icon = ""
+					highlight = "DiagnosticError"
+				elseif diagnostic.severity == 2 then
+					icon = ""
+					highlight = "DiagnosticWarn"
+				elseif diagnostic.severity == 3 then
+					icon = ""
+					highlight = "DiagnosticInfo"
+				elseif diagnostic.severity == 4 then
+					icon = ""
+					highlight = "DiagnosticHint"
+				end
+				return i .. "/" .. total .. " " .. icon .. "  ", highlight
 			end
-		return diagnostic.message
-	end
-end
-local function prefix(diagnostic, i, total)
-	local icon, highlight
-	if diagnostic.severity == 1 then
-		icon = ""
-		highlight = "DiagnosticError"
-	elseif diagnostic.severity == 2 then
-		icon = ""
-		highlight = "DiagnosticWarn"
-	elseif diagnostic.severity == 3 then
-		icon = ""
-		highlight = "DiagnosticInfo"
-	elseif diagnostic.severity == 4 then
-		icon = ""
-		highlight = "DiagnosticHint"
-	end
-	return i .. "/" .. total .. " " .. icon .. "  ", highlight
-end
-local function wrap_options(custom, handler)
-	return function(opts)
-		opts = opts and vim.tbl_extend(opts, custom) or custom
-		if type(handler) == "string" then
-			require("telescope.builtin")[handler](opts)
-		else
-			handler(opts)
-		end
-	end
-end
-
-
--- Diagnostics Setup
-vim.diagnostic.config({
-	signs = false,
-	virtual_text = false,
-	update_in_insert = false,
-	underline = true,
-	float = {
-		focusable = false,
-		border = border,
-		scope = "cursor",
-		source = "if_many",
-		 format = format,
-		header = { "Cursor Diagnostics: ", "DiagnosticInfo" },
-		pos = 1,
-		prefix = prefix,
-	},
-})
-
--- Hover
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-	border = border,
-})
-
--- Signature Help
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-	border = border,
-})
-
--- Reference
--- Reference: https://github.com/rcarriga/dotfiles/blob/master/.config/nvim/lua/config/lsp/handlers.lua#L16
-vim.lsp.handlers["textDocument/references"] = wrap_options({ layout_strategy = "vertical" }, "lsp_references")
-
--- Document Symboll
--- Reference: https://github.com/rcarriga/dotfiles/blob/master/.config/nvim/lua/config/lsp/handlers.lua#L20
-vim.lsp.handlers["textDocument/documentSymbol"] = require("telescope.builtin").lsp_document_symbols
-
--- Defination
-local function goto_definition(split_cmd)
-	local util = vim.lsp.util
-	local log = require("vim.lsp.log")
-	local api = vim.api
-
-	local handler = function(_, result, ctx)
-		if result == nil or vim.tbl_isempty(result) then
-			local _ = log.info() and log.info(ctx.method, "No location found")
-			return nil
-		end
-
-		if split_cmd then
-			vim.cmd(split_cmd)
-		end
-
-		if vim.tbl_islist(result) then
-			util.jump_to_location(result[1])
-
-			if #result > 1 then
-				util.set_qflist(util.locations_to_items(result))
-				api.nvim_command("copen")
-				api.nvim_command("wincmd p")
+			local function wrap_options(custom, handler)
+				return function(opts)
+					opts = opts and vim.tbl_extend(opts, custom) or custom
+					if type(handler) == "string" then
+						require("telescope.builtin")[handler](opts)
+					else
+						handler(opts)
+					end
+				end
 			end
-		else
-			util.jump_to_location(result)
-		end
-	end
 
-	return handler
-end
+			-- Diagnostics Setup
+			vim.diagnostic.config({
+				signs = false,
+				virtual_text = false,
+				update_in_insert = false,
+				underline = true,
+				float = {
+					focusable = false,
+					border = border,
+					scope = "cursor",
+					source = "if_many",
+					header = { "Cursor Diagnostics: ", "DiagnosticInfo" },
+					pos = 1,
+					prefix = prefix,
+				},
+			})
 
-vim.lsp.handlers["textDocument/definition"] = goto_definition("split")
+			-- Hover
+			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+				border = border,
+			})
+
+			-- Signature Help
+			vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+				border = border,
+			})
+
+			-- Reference
+			-- Reference: https://github.com/rcarriga/dotfiles/blob/master/.config/nvim/lua/config/lsp/handlers.lua#L16
+			vim.lsp.handlers["textDocument/references"] =
+				wrap_options({ layout_strategy = "vertical" }, "lsp_references")
+
+			-- Document Symboll
+			-- Reference: https://github.com/rcarriga/dotfiles/blob/master/.config/nvim/lua/config/lsp/handlers.lua#L20
+			vim.lsp.handlers["textDocument/documentSymbol"] = require("telescope.builtin").lsp_document_symbols
+
+			-- Defination
+			local function goto_definition(split_cmd)
+				local util = vim.lsp.util
+				local log = require("vim.lsp.log")
+				local api = vim.api
+
+				local handler = function(_, result, ctx)
+					if result == nil or vim.tbl_isempty(result) then
+						local _ = log.info() and log.info(ctx.method, "No location found")
+						return nil
+					end
+
+					if split_cmd then
+						vim.cmd(split_cmd)
+					end
+
+					if vim.tbl_islist(result) then
+						util.jump_to_location(result[1])
+
+						if #result > 1 then
+							util.set_qflist(util.locations_to_items(result))
+							api.nvim_command("copen")
+							api.nvim_command("wincmd p")
+						end
+					else
+						util.jump_to_location(result)
+					end
+				end
+
+				return handler
+			end
+
+			vim.lsp.handlers["textDocument/definition"] = goto_definition("split")
 		end,
 	},
 }
