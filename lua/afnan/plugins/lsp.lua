b793dd9c088000648bff792174765ac28aa67369
Afnan <afnanmalik2712@gmail.com>
2024-07-20 17:28:33 +0530

    feat(lsp): Added autoformatting for lua files

--- lua/afnan/plugins/lsp.lua
+++ lua/afnan/plugins/lsp.lua
@@ -1,291 +1,301 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		ft = { "lua", "markdown", "json" },
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"b0o/schemastore.nvim",
-			{
-				"folke/trouble.nvim",
-				opts = {},
-				cmd = "Trouble",
-			},
-			{
-				"simrat39/symbols-outline.nvim",
-				cmd = "SymbolsOutline",
-				config = function()
-					require("symbols-outline").setup()
-				end,
-			},
-			{
-				"folke/lazydev.nvim",
-				ft = "lua",
-				opts = {
-					library = {
-						{ path = "luvit-meta/library", words = { "vim%.uv" } },
-					},
-				},
-			},
-			{ "Bilal2453/luvit-meta", lazy = true },
-			{ "bfredl/nvim-luadev", ft = { "lua" }, cmd = { "Luadev" } },
-			-- { mizio/typescript-tools.nvim" },
-			-- {
-			--    "mfussenegger/nvim-jdtls",
-			--    ft = { "java" },
-			--    config = function()
-			--       require("jdtls").start_or_attach({
-			--          cmd = { "/data/data/com.termux/files/home/jdtls/start.sh" },
-			--          root_dir = require("jdtls.setup").find_root({ ".git" }),
-			--          filetypes = { "java" },
-			--       })
-			--    end,
-			-- },
-		},
-		config = function()
-			local cmp_nvim_lsp = require("cmp_nvim_lsp")
-			local lspconfig = require("lspconfig")
-			local capabilities = cmp_nvim_lsp.default_capabilities()
+   {
+      "neovim/nvim-lspconfig",
+      ft = { "lua", "markdown", "json" },
+      dependencies = {
+         "hrsh7th/cmp-nvim-lsp",
+         "b0o/schemastore.nvim",
+         {
+            "folke/trouble.nvim",
+            opts = {},
+            cmd = "Trouble",
+         },
+         {
+            "simrat39/symbols-outline.nvim",
+            cmd = "SymbolsOutline",
+            config = function()
+               require("symbols-outline").setup()
+            end,
+         },
+         {
+            "folke/lazydev.nvim",
+            ft = "lua",
+            opts = {
+               library = {
+                  { path = "luvit-meta/library", words = { "vim%.uv" } },
+               },
+            },
+         },
+         { "Bilal2453/luvit-meta", lazy = true },
+         { "bfredl/nvim-luadev",   ft = { "lua" }, cmd = { "Luadev" } },
+         -- { mizio/typescript-tools.nvim" },
+         -- {
+         --    "mfussenegger/nvim-jdtls",
+         --    ft = { "java" },
+         --    config = function()
+         --       require("jdtls").start_or_attach({
+         --          cmd = { "/data/data/com.termux/files/home/jdtls/start.sh" },
+         --          root_dir = require("jdtls.setup").find_root({ ".git" }),
+         --          filetypes = { "java" },
+         --       })
+         --    end,
+         -- },
+      },
+      config = function()
+         local cmp_nvim_lsp = require("cmp_nvim_lsp")
+         local lspconfig = require("lspconfig")
+         local capabilities = cmp_nvim_lsp.default_capabilities()
 
-			local function on_attach(client, bufnr)
-				local function set_keymap(mode, lhs, rhs)
-					vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, { silent = true, noremap = true })
-				end
+         local function on_attach(client, bufnr)
+            local function set_keymap(mode, lhs, rhs)
+               vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, { silent = true, noremap = true })
+            end
 
-				set_keymap("n", "K", ":lua vim.lsp.buf.hover()<CR>")
-				set_keymap("n", "gd", ":lua vim.lsp.buf.definition()<CR>")
-				set_keymap("n", "gI", ":lua vim.lsp.buf.implementation()<CR>")
-				set_keymap("n", "gk", ":lua vim.diagnostic.goto_next()<CR>")
-				set_keymap("n", "gj", ":lua vim.diagnostic.goto_prev()<CR>")
-				set_keymap("n", "gR", ":lua vim.lsp.buf.references()<CR>")
-				set_keymap("n", "gr", ":lua vim.lsp.buf.rename()<CR>")
-				set_keymap("n", "ga", ":lua vim.lsp.buf.code_action()<CR>")
-				set_keymap("i", "<C-s>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
-				set_keymap("n", "gs", ":SymbolsOutline<CR>")
-				print("Language Server: " .. client.name .. " is started!")
+            set_keymap("n", "K", ":lua vim.lsp.buf.hover()<CR>")
+            set_keymap("n", "gd", ":lua vim.lsp.buf.definition()<CR>")
+            set_keymap("n", "gD", ":lua vim.lsp.buf.declaration()<CR>")
+            set_keymap("n", "gI", ":lua vim.lsp.buf.implementation()<CR>")
+            set_keymap("n", "gk", ":lua vim.diagnostic.goto_next()<CR>")
+            set_keymap("n", "gj", ":lua vim.diagnostic.goto_prev()<CR>")
+            set_keymap("n", "gR", ":lua vim.lsp.buf.references()<CR>")
+            set_keymap("n", "gr", ":lua vim.lsp.buf.rename()<CR>")
+            set_keymap("n", "ga", ":lua vim.lsp.buf.code_action()<CR>")
+            set_keymap("i", "<C-s>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+            set_keymap("n", "gs", ":SymbolsOutline<CR>")
+            print("Language Server: " .. client.name .. " is started!")
 
-				if client.supports_method("textDocument/inlayHint") then
-					vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-               vim.api.nvim_set_hl(0, 'LSPInlayHint', { fg = "red"})
-				else
+            if client.supports_method("textDocument/inlayHint") then
+               vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+               vim.api.nvim_set_hl(0, 'LSPInlayHint', { fg = "red" })
+            else
                print(client.name .. " does not support textDocument/inlayHint")
-				end
-			end
+            end
 
-			-- JAVASCRIPT
-			-- lspconfig.tsserver.setup({
-			--    on_attach = on_attach,
-			--    capabilities = capabilities,
-			--    settings = {
-			--       completions = {
-			--          completeFunctionCalls = true,
-			--       },
-			--    },
-			--    init_options = {
-			--       preferences = {
-			--          includeCompletionsWithSnippetText = true,
-			--          includeCompletionsForImportStatements = true,
-			--       },
-			--    },
-			--    single_file_support = true,
-			-- })
+            if client.name == "lua_ls" then
+               vim.api.nvim_create_autocmd("BufWritePre", {
+                  desc = "Auto formatting",
+                  callback = function()
+                     return vim.lsp.buf.format()
+                  end
+               })
+            end
+         end
 
-			-- JSON
-			lspconfig.jsonls.setup({
-				on_attach = on_attach,
-				capabilities = capabilities,
-				init_options = { provideFormatter = false },
-				single_file_support = true,
-				settings = {
-					json = { schemas = require("schemastore").json.schemas() },
-				},
-			})
+         -- JAVASCRIPT
+         -- lspconfig.tsserver.setup({
+         --    on_attach = on_attach,
+         --    capabilities = capabilities,
+         --    settings = {
+         --       completions = {
+         --          completeFunctionCalls = true,
+         --       },
+         --    },
+         --    init_options = {
+         --       preferences = {
+         --          includeCompletionsWithSnippetText = true,
+         --          includeCompletionsForImportStatements = true,
+         --       },
+         --    },
+         --    single_file_support = true,
+         -- })
 
-			-- HTML & CSS
-			-- lspconfig.cssls.setup({
-			--    on_attach = on_attach,
-			--    capabilities = capabilities,
-			-- })
-			-- lspconfig.html.setup({
-			--    on_attach = on_attach,
-			--    capabilities = capabilities,
-			-- })
+         -- JSON
+         lspconfig.jsonls.setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            init_options = { provideFormatter = false },
+            single_file_support = true,
+            settings = {
+               json = { schemas = require("schemastore").json.schemas() },
+            },
+         })
 
-			local runtime_path = vim.split(package.path, ";")
-			table.insert(runtime_path, "lua/?.lua")
-			table.insert(runtime_path, "lua/?/init.lua")
+         -- HTML & CSS
+         -- lspconfig.cssls.setup({
+         --    on_attach = on_attach,
+         --    capabilities = capabilities,
+         -- })
+         -- lspconfig.html.setup({
+         --    on_attach = on_attach,
+         --    capabilities = capabilities,
+         -- })
 
-			-- LUA
-			lspconfig.lua_ls.setup({
-				on_attach = on_attach,
-				capabilities = capabilities,
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim", "it", "describe", "before_each", "self" },
-							disable = { "trailing-space", "deprecated", "lowercase-global" },
-						},
-						runtime = { version = "LuaJIT", path = runtime_path },
-						workspace = {
-							checkTHirdParty = false,
-							--[[  vim.api.nvim_get_runtime_file("", true) ]]
-							library = {
-								[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+         local runtime_path = vim.split(package.path, ";")
+         table.insert(runtime_path, "lua/?.lua")
+         table.insert(runtime_path, "lua/?/init.lua")
 
-								[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-								[vim.fn.stdpath("config") .. "/lua"] = true,
-							},
-						},
-					},
-					completion = {
-						showWord = "Disable",
-						callSnippet = "Replace",
-					},
-					IntelliSense = {
-						traceBeSetted = true,
-						traceFieldInject = true,
-						traceLocalSet = true,
-						traceReturn = true,
-					},
-					hint = {
-						arrayIndex = "Enable",
-						enable = true,
-						paramName = "All",
-						paramType = true,
-						setType = true,
-					},
-					hover = {
-						enable = true,
-						enumsLimit = 5,
-						previewFields = 20,
-						viewNumber = true,
-						viewString = true,
-						viewStringMax = 1000,
-					},
-					misc = { parameters = { "self" } },
-					semantic = {
-						annotation = true,
-						enable = true,
-						keyword = true,
-						variable = true,
-					},
-					window = { progressBar = true, statusBar = true },
-					codeLens = { enable = true },
-					-- telemetry = { enable = true },
-				},
-			})
+         -- LUA
+         lspconfig.lua_ls.setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            settings = {
+               Lua = {
+                  diagnostics = {
+                     globals = { "vim", "it", "describe", "before_each", "self" },
+                     disable = { "trailing-space", "deprecated", "lowercase-global" },
+                  },
+                  runtime = { version = "LuaJIT", path = runtime_path },
+                  workspace = {
+                     checkTHirdParty = false,
+                     --[[  vim.api.nvim_get_runtime_file("", true) ]]
+                     library = {
+                        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
 
-			-- EMMET
-			-- lspconfig.emmet_language_server.setup({
-			--    on_attach = on_attach,
-			--    capabilities = capabilities,
-			--    filetypes = { "css", "html", "jsx", "tsx", "javascript", "javascriptreact" },
-			-- })
+                        [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+                        [vim.fn.stdpath("config") .. "/lua"] = true,
+                     },
+                  },
+               },
+               completion = {
+                  showWord = "Disable",
+                  callSnippet = "Replace",
+               },
+               IntelliSense = {
+                  traceBeSetted = true,
+                  traceFieldInject = true,
+                  traceLocalSet = true,
+                  traceReturn = true,
+               },
+               hint = {
+                  arrayIndex = "Enable",
+                  enable = true,
+                  paramName = "All",
+                  paramType = true,
+                  setType = true,
+               },
+               hover = {
+                  enable = true,
+                  enumsLimit = 5,
+                  previewFields = 20,
+                  viewNumber = true,
+                  viewString = true,
+                  viewStringMax = 1000,
+               },
+               misc = { parameters = { "self" } },
+               semantic = {
+                  annotation = true,
+                  enable = true,
+                  keyword = true,
+                  variable = true,
+               },
+               window = { progressBar = true, statusBar = true },
+               codeLens = { enable = true },
+               -- telemetry = { enable = true },
+            },
+         })
 
-			-- CLANGD
-			--[[ lspconfig.jdtls.setup({
+         -- EMMET
+         -- lspconfig.emmet_language_server.setup({
+         --    on_attach = on_attach,
+         --    capabilities = capabilities,
+         --    filetypes = { "css", "html", "jsx", "tsx", "javascript", "javascriptreact" },
+         -- })
+
+         -- CLANGD
+         --[[ lspconfig.jdtls.setup({
 				on_attach = on_attach,
 				capabilities = capabilities,
 				cmd = { "/data/data/com.termux/files/home/jdtls/start.sh" },
 				autostart = true,
 			}) ]]
 
-			local function prefix(diagnostic, i, total)
-				local icon, highlight
-				if diagnostic.severity == 1 then
-					icon = ""
-					highlight = "DiagnosticError"
-				elseif diagnostic.severity == 2 then
-					icon = ""
-					highlight = "DiagnosticWarn"
-				elseif diagnostic.severity == 3 then
-					icon = ""
-					highlight = "DiagnosticInfo"
-				elseif diagnostic.severity == 4 then
-					icon = ""
-					highlight = "DiagnosticHint"
-				end
-				return i .. "/" .. total .. " " .. icon .. "  ", highlight
-			end
+         local function prefix(diagnostic, i, total)
+            local icon, highlight
+            if diagnostic.severity == 1 then
+               icon = ""
+               highlight = "DiagnosticError"
+            elseif diagnostic.severity == 2 then
+               icon = ""
+               highlight = "DiagnosticWarn"
+            elseif diagnostic.severity == 3 then
+               icon = ""
+               highlight = "DiagnosticInfo"
+            elseif diagnostic.severity == 4 then
+               icon = ""
+               highlight = "DiagnosticHint"
+            end
+            return i .. "/" .. total .. " " .. icon .. "  ", highlight
+         end
 
-			local function wrap_options(custom, handler)
-				return function(opts)
-					opts = opts and vim.tbl_extend(opts, custom) or custom
-					if type(handler) == "string" then
-						require("telescope.builtin")[handler](opts)
-					else
-						handler(opts)
-					end
-				end
-			end
+         local function wrap_options(custom, handler)
+            return function(opts)
+               opts = opts and vim.tbl_extend(opts, custom) or custom
+               if type(handler) == "string" then
+                  require("telescope.builtin")[handler](opts)
+               else
+                  handler(opts)
+               end
+            end
+         end
 
-			-- Diagnostics Setup
-			vim.diagnostic.config({
-				signs = false,
-				virtual_text = false,
-				update_in_insert = false,
-				underline = true,
-				float = {
-					focusable = false,
-					border = "rounded",
-					scope = "cursor",
-					source = "if_many",
-					header = { "Cursor Diagnostics: ", "DiagnosticInfo" },
-					prefix = prefix,
-				},
-			})
+         -- Diagnostics Setup
+         vim.diagnostic.config({
+            signs = false,
+            virtual_text = false,
+            update_in_insert = false,
+            underline = true,
+            float = {
+               focusable = false,
+               border = "rounded",
+               scope = "cursor",
+               source = "if_many",
+               header = { "Cursor Diagnostics: ", "DiagnosticInfo" },
+               prefix = prefix,
+            },
+         })
 
-			-- Hover
-			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-				border = "rounded",
-			})
+         -- Hover
+         vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+            border = "rounded",
+         })
 
-			-- Signature Help
-			vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-				border = "rounded",
-			})
+         -- Signature Help
+         vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+            border = "rounded",
+         })
 
-			-- Reference
-			-- Reference: https://github.com/rcarriga/dotfiles/blob/master/.config/nvim/lua/config/lsp/handlers.lua#L16
-			vim.lsp.handlers["textDocument/references"] =
-				wrap_options({ layout_strategy = "vertical" }, "lsp_references")
+         -- Reference
+         -- Reference: https://github.com/rcarriga/dotfiles/blob/master/.config/nvim/lua/config/lsp/handlers.lua#L16
+         vim.lsp.handlers["textDocument/references"] =
+             wrap_options({ layout_strategy = "vertical" }, "lsp_references")
 
-			-- Document Symboll
-			-- Reference: https://github.com/rcarriga/dotfiles/blob/master/.config/nvim/lua/config/lsp/handlers.lua#L20
-			vim.lsp.handlers["textDocument/documentSymbol"] = require("telescope.builtin").lsp_document_symbols
+         -- Document Symboll
+         -- Reference: https://github.com/rcarriga/dotfiles/blob/master/.config/nvim/lua/config/lsp/handlers.lua#L20
+         vim.lsp.handlers["textDocument/documentSymbol"] = require("telescope.builtin").lsp_document_symbols
 
-			-- Defination
-			local function goto_definition(split_cmd)
-				local util = vim.lsp.util
-				local log = require("vim.lsp.log")
-				local api = vim.api
+         -- Defination
+         local function goto_definition(split_cmd)
+            local util = vim.lsp.util
+            local log = require("vim.lsp.log")
+            local api = vim.api
 
-				local handler = function(_, result, ctx)
-					if result == nil or vim.tbl_isempty(result) then
-						local _ = log.info() and log.info(ctx.method, "No location found")
-						return nil
-					end
+            local handler = function(_, result, ctx)
+               if result == nil or vim.tbl_isempty(result) then
+                  local _ = log.info() and log.info(ctx.method, "No location found")
+                  return nil
+               end
 
-					if split_cmd then
-						vim.cmd(split_cmd)
-					end
+               if split_cmd then
+                  vim.cmd(split_cmd)
+               end
 
-					if vim.tbl_islist(result) then
-						util.jump_to_location(result[1], "utf-8", true)
+               if vim.tbl_islist(result) then
+                  util.jump_to_location(result[1], "utf-8", true)
 
-						if #result > 1 then
-							vim.fn.setqflist(util.locations_to_items(result, "utf-8"))
-							api.nvim_command("copen")
-							api.nvim_command("wincmd p")
-						end
-					else
-						util.jump_to_location(result , "utf-8", true)
-					end
-				end
+                  if #result > 1 then
+                     vim.fn.setqflist(util.locations_to_items(result, "utf-8"))
+                     api.nvim_command("copen")
+                     api.nvim_command("wincmd p")
+                  end
+               else
+                  util.jump_to_location(result, "utf-8", true)
+               end
+            end
 
-				return handler
-			end
+            return handler
+         end
 
-			vim.lsp.handlers["textDocument/definition"] = goto_definition("vsplit")
-		end,
-	},
+         vim.lsp.handlers["textDocument/definition"] = goto_definition("vsplit")
+      end,
+   },
 }
