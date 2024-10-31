3777dbffabf1b3a3adf9aa5f7514b9859eeb7b66
HyperAfnan <afnanmalik2712@gmail.com>
2024-10-31 13:01:14 +0530

    Add config for

--- lua/afnan/plugins/presistence.lua
+++ lua/afnan/plugins/presistence.lua
@@ -1,5 +1,15 @@
 return {
-	"folke/persistence.nvim",
-	event = "BufReadPre",
-	config = function() end,
+   "folke/persistence.nvim",
+   event = "BufReadPre",
+   keys = {
+      { ",qs", function() require("persistence").load() end },
+      {",qS", function() require("persistence").select() end},
+      {",ql", function() require("persistence").load({ last = true }) end},
+      {",qd", function() require("persistence").stop() end},
+   },
+   opts = {
+      dir = vim.fn.stdpath("state") .. "/sessions/",
+      need = 0,
+      branch = false,
+   },
 }
